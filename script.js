a53baf7343dd4a55e22e9634af44d88a1de2cb04
unknown <ebunoluwafolorunsho541@gmail.com>
2025-10-22 12:56:42 -0800

    project

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const things = [
   {
-    image: '<img src="/images/tele.svg"> ',
+    image: '<img src="./images/tele.svg"> ',
     heading: "Digital Products",
     desc: "Sell any and every kind of digital product, from content packs to designs to bundles and more without stress.",
   },
   {
-    image: '<img src="/images/eee.svg"> ',
+    image: '<img src="./images/eee.svg"> ',
     heading: "Ebooks",
     desc: "Selar is the best platform to sell your ebooks both downloadable and non-downloadable in any format.",
   },
   {
-    image: '<img src="/images/book.svg"> ',
+    image: '<img src="./images/book.svg"> ',
     heading: "Courses and Membership",
     desc: "You can host your courses & membership sites with unlimited videos & files, unlimited storage, and have unlimited students, plus you get content security to prevent theft.",
   },
   {
-    image: '<img src="/images/side.svg"> ',
+    image: '<img src="./images/side.svg"> ',
     heading: "Event Tickets and Training",
     desc: "Sell tickets for all kinds of events and access to masterclasses, events, workshops, training, webinars, and even more.",
   },
   {
-    image: '<img src="/images/star.svg"> ',
+    image: '<img src="./images/star.svg"> ',
     heading: "Services",
     desc: "Sell any kind of service, from coaching services to consultations to counseling sessions to design services and more.",
   },
   {
-    image: '<img src="/images/box.svg"> ',
+    image: '<img src="./images/box.svg"> ',
     heading: "Physical Goods",
     desc: "You can also use Selar to sell your physical product from clothing to books to electronics and appliances and more.",
   },
@@ -47,21 +47,21 @@ document.getElementById("demo").innerHTML = products;
 
 const pupil = [
   {
-    image: '<img src="/images/affilate.svg" alt=""> ',
+    image: '<img src="./images/affilate.svg" alt=""> ',
     heading: "Affiliates",
     desc: "Set up your own affiliate marketing system, and give a commission for anyone that facilitates a sale for your store.",
     link: '<a href="#" >Learn More</a>',
   },
 
   {
-    image: '<img src="/images/master.svg" alt=""> ',
+    image: '<img src="./images/master.svg" alt=""> ',
     heading: "Sales Page",
     desc: "Create custom sales/landing pages for your products. Descriptive sales pages drive more conversions.",
     link: '<a href="#" >Learn More</a>',
   },
 
   {
-    image: '<img src="/images/automate.svg" alt=""> ',
+    image: '<img src="./images/automate.svg" alt=""> ',
     heading: "Automated Follow-ups",
     desc: "Never miss a prospective buyer with Selar's automated follow ups. Proven to drive conversions by at least 30%.",
     link: '<a href="#" >Learn More</a>',
